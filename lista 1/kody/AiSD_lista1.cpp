cc0b7e2a49b92e651e6936b64d3023c3fc14a227
Maciej Kmieciak <277516@student.pwr.edu.pl>
2024-11-06 20:22:47 +0100

    Test

---
+++ lista 1/kody/AiSD_lista1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Porownanie algorytmow INSERTION_SORT, MERGE_SORT i HEAP_SORT" << endl;
+    cout << "Maciej Kmieciak 277516" << endl;
+
+
+    
+    return 0;
+}
